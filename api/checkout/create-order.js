148e48f2a7c1cf5f0d889d8fbed80d2089a990a4
alexis <alexis.nomine9999@gmail.com>
2026-02-21 22:45:41 +0100

    modif du setcors

--- api/checkout/create-order.js
+++ api/checkout/create-order.js
@@ -2,18 +2,8 @@
  * Proxy Vercel : POST /api/checkout/create-order → WordPress custom-checkout/v1/create-order
  */
 
-const ALLOWED_ORIGINS = [
-  "https://www.impexo.fr",
-  "https://impexo.fr",
-  "http://localhost:5173",
-  "http://localhost:3000",
-  "http://127.0.0.1:5173",
-];
-
 function setCors(res, req) {
-  const origin = req?.headers?.origin ?? "https://www.impexo.fr";
-  const allow = ALLOWED_ORIGINS.includes(origin) ? origin : ALLOWED_ORIGINS[0];
-  res.setHeader("Access-Control-Allow-Origin", allow);
+  res.setHeader("Access-Control-Allow-Origin", req.headers?.origin || "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", "true");
